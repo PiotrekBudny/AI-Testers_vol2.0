6c30a46a89153ca68ac1913bafc2020ef034121a
Piotrek Budny <piotrek.budny@gmail.com>
2026-08-31 18:53:23 +0200

    Commit adjusting tests to Rolnopol

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
+  timeout: 10_000,
   fullyParallel: true,
-  reporter: [['html', {open: 'never'}]],
+  reporter: [["html", { open: "never" }]],
   use: {
-    trace: 'on',
-    baseURL: 'http://localhost:3000/'
+    trace: "on",
+    baseURL: "http://localhost:3000/",
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    }
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
   ],
 });
--- tests/1_designing_framework_and_processes_with_AI/example.spec.ts
+++ tests/1_designing_framework_and_processes_with_AI/example.spec.ts
@@ -1,20 +1,24 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Rolnopol/);
+
+  await page.close();
 });
 
 test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("/");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  await page.getByRole("link", { name: "Get Started Free" }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading for account creation.
   await expect(
-    page.getByRole("heading", { name: "Installation" }),
+    page.getByRole("heading", { name: "Create Your User Account" }),
   ).toBeVisible();
+
+  await page.close();
 });
